995062f9336cec6908aece275aae68a0a2d64193
Mark Young <mark_young@hotmail.com>
2026-02-19 23:00:52 +0000

    fix: redirect error on clicking like button

--- app/controllers/pics_controller.rb
+++ app/controllers/pics_controller.rb
@@ -41,7 +41,7 @@ class PicsController < ApplicationController
 
   def upvote
     @pic.upvote_by current_user
-    redirect_to :back
+    redirect_to request.referer || root_url
   end
 
   private
